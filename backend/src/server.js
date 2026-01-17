8c8bbc67edf494695641cc2aa2641f14eca1416a
Ron-ron Aspe Rivera <ronaspe42@gmail.com>
2026-01-17 16:20:01 +0800

    fix some issues in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import "dotenv/config"
-import cors from "cors";
-import path from "path";
+import cors from "cors"
+import path from "path"
 import cookieParser from "cookie-parser";
 
 import { ENV } from "./lib/env.js";
@@ -9,49 +9,56 @@ import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.routes.js";
 import jobsRoutes from "./routes/jobs.routes.js";
-import applicationRoutes from "./routes/application.routes.js";
-
-import serverless from "serverless-http"; // optional
+import applicationRoutes from "./routes/application.routes.js"
 
 const app = express();
+
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
+
+if(!ENV.CLIENT_URL){
+    console.log("CLIENT_URL was not defined!");
+}
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: ENV.CLIENT_URL,
     credentials: true
-}));
+}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/jobs", jobsRoutes);
-app.use("/api/applications", applicationRoutes);
+app.use("/api/jobs", applicationRoutes);
 
-// Optional: serve frontend if in production and running locally
-if (!ENV.SERVERLESS && ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    app.get(/.*/, (_, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-    });
+if(!ENV.NODE_ENV){
+    console.log("NODE_ENV was not defined");
 }
 
-// Dynamic: local server vs serverless
-if (!ENV.SERVERLESS) {
-    // Run normal server for local testing
-    const startServer = async () => {
-        try {
+if(!ENV.SERVERLESS){
+    if(ENV.NODE_ENV === "production"){
+        app.use(express.static(path.join(__dirname, "../frontend/dist")));
+        app.get(/.*/, (_, res) =>{
+            res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+        });
+    }
+
+    const startServer = async () =>{
+        try{
             await connectDB();
-            app.listen(PORT, () => {
-                console.log("App listening on PORT:", PORT);
-            });
-        } catch (error) {
-            console.error("Failed to start server:", error);
+            app.listen(PORT, () =>{
+                console.log("App is listening to PORT: ", PORT);
+            })
         }
-    };
-    startServer();
-}
-// Export for serverless platforms like Vercel
-export default serverless(app);
+        catch(error){
 
+        }
+    }
+
+    startServer();
+
+
+}
+
+export default app;
